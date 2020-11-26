bfd7dad8388b9144ec23fe66aedb7655b823a3a8
Shuyang He <maxhe2019@gmail.com>
2020-11-25 23:35:26 -0800

    submenu handle

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,10 @@
       .classList.toggle("nav-banner--active");
   }
 
+  function handleSubMenu() {
+    document.querySelectorAll("submenu");
+  }
+
   document.querySelector(".menu").addEventListener(
     "click",
     () => {
@@ -36,4 +40,28 @@
     },
     false
   );
+
+  const pagehead__drop = document.querySelectorAll(".pagehead__drop > li");
+  for (let li of pagehead__drop) {
+    li.addEventListener(
+      "mouseenter",
+      () => {
+        const submenu = li.querySelector(".submenu");
+        if (submenu) {
+          submenu.classList.toggle("submenu--active");
+        }
+      },
+      false
+    );
+    li.addEventListener(
+      "mouseleave",
+      () => {
+        const submenu = li.querySelector(".submenu");
+        if (submenu) {
+          submenu.classList.toggle("submenu--active");
+        }
+      },
+      false
+    );
+  }
 })();
