2f25258e643eb504c6e925275c60927e09021c9f
Shuyang He <maxhe2019@gmail.com>
2020-11-25 21:23:05 -0800

    get rid of unused code from js

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,6 @@
 (function () {
   "use strict";
 
-  // let widthThreshold = false;
-
-  // window.onresize = () => {
-  //   if (document.body.clientWidth <= 1300 && widthThreshold) {
-  //     document.querySelector(".pagehead").classList.toggle("container");
-  //     widthThreshold = !widthThreshold;
-  //   }
-  //   if (document.body.clientWidth > 1300 && !widthThreshold) {
-  //     document.querySelector(".pagehead").classList.toggle("container");
-  //     widthThreshold = !widthThreshold;
-  //   }
-  // };
-
   function handleMenu() {
     document.querySelector(".menu").classList.toggle("menu--active");
   }
